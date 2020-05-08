87be09878b98e633a2c32288b2dd74666aa3bde1
sunjiankun02 <sunjiankun02@gmail.com>
2020-05-08 23:16:41 +0100

    plot1

--- plot1.R
+++ plot1.R
@@ -17,7 +17,7 @@ data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
 data$Time <- strptime(paste(data$Date, data$Time), format = "%Y-%m-%d %H:%M:%S")
 
 # Draw plot1 #
-png(file = "plot1.png")
+png(file = "plot1.png", width = 480, height = 480)
 par(mfrow = c(1,1))
 hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
 dev.off()
